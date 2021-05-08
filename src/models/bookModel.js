3bc916e7db73c56cb23b16134f78378de551d282
Andrew_vdp <andriy.v.dp@gmail.com>
2021-05-08 21:34:40 +0300

    add two fields to bookModel

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -2,11 +2,13 @@ import mongoose from 'mongoose'
 
 const {Schema, model} = mongoose
 
-const schema = new Schema({
+const bookSchema = new Schema({
   title: {type: String, required:true},
   author: {type: String, required:true},
   category: {type: String, required:true},
   price: {type: Number, required:true},
+  description: {type: String, required: false},
+  imageLink: {type: String, required: false}
 })
 
-export default model('Books', schema)
+export default model('Books', bookSchema)
