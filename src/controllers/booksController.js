fc92c9b8967aadb18cfa643176acd220488fe43a
Andrew_vdp <andriy.v.dp@gmail.com>
2021-05-18 11:45:08 +0300

    refactor filterBooks function in booksController.js

--- src/controllers/booksController.js
+++ src/controllers/booksController.js
@@ -35,17 +35,19 @@ async function searchBooks(req, res) {
 
 async function filterBooks(req, res) {
   try {
-    const filterCategories = req.query.categories === '' ? null : req.query.categories.replace('_', '')
+    const filterCategories = req.query.categories === '' ? null : req.query.categories.split('_')
     const filterPrice = req.query.price === '' ? null : req.query.price
 
     let filteredBooks = []
 
+    const sortBy = filterPrice === 'cheap' ? 'asc' : 'desc'
+    const regexForCategories = new RegExp(`^${filterCategories.join('|')}$`, 'i')
+
     if(filterCategories && filterPrice){
-      filteredBooks = await BooksDB.find({categories: new RegExp(req.query.value, 'i')}).sort({price: sortBy})
+      filteredBooks = await BooksDB.find({category: regexForCategories}).sort({price: sortBy})
     } else if(filterCategories) {
-      filteredBooks = await BooksDB.find({categories: new RegExp(req.query.value, 'i')})
+      filteredBooks = await BooksDB.find({category: regexForCategories})
     } else if(filterPrice) {
-      const sortBy = filterPrice === 'cheap' ? 'asc' : 'desc'
       filteredBooks = await BooksDB.find({}).sort({price: sortBy})
     }
 
